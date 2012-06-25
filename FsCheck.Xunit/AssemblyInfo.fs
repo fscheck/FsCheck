439f57611f676e84dfa52a0edf85dcead0daa7d6
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2012-06-24 20:55:28 -0400

    Setup for 0.8 release.
    - Added updated and new nuspec. Takes as much as possible from AssemblyInfo now.
    - Updated Makefile
    - Added AssemblyInfo.fs for FsCheck.Xunit

---
+++ FsCheck.Xunit/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿namespace FsCheck
+
+open System.Reflection
+
+[<assembly:AssemblyDescription("FsCheck.Xunit")>]
+[<assembly:AssemblyCompany("Kurt Schelfthout")>]
+[<assembly:AssemblyTitle("FsCheck.Xunit")>]
+[<assembly:AssemblyProduct("FsCheck.Xunit")>]
+[<assembly:AssemblyVersion("0.1")>]
+[<assembly:AssemblyFileVersion("0.1")>]
+
+[<assembly: System.Runtime.InteropServices.ComVisible(false)>]
+[<assembly: System.CLSCompliant(true)>]
+
+do()
+
--- FsCheck/AssemblyInfo.fs
+++ FsCheck/AssemblyInfo.fs
@@ -2,16 +2,12 @@
 
 open System.Reflection
 
-module private Constant =
-    [<Literal>] 
-    let version = "0.8.0.0"
-
-[<assembly:AssemblyDescription("FsCheck.dll")>]
-[<assembly:AssemblyCompany("FsCheck CodePlex Project")>]
-[<assembly:AssemblyTitle("FsCheck.dll")>]
-[<assembly:AssemblyProduct("FsCheck random testing framework")>]
-[<assembly:AssemblyVersion(Constant.version)>]
-[<assembly:AssemblyFileVersion(Constant.version)>]
+[<assembly:AssemblyDescription("FsCheck")>]
+[<assembly:AssemblyCompany("Kurt Schelfthout")>]
+[<assembly:AssemblyTitle("FsCheck")>]
+[<assembly:AssemblyProduct("FsCheck")>]
+[<assembly:AssemblyVersion("0.8.0.0")>]
+[<assembly:AssemblyFileVersion("0.8.0.0")>]
 
 [<assembly: System.Runtime.InteropServices.ComVisible(false)>]
 [<assembly: System.CLSCompliant(true)>]
