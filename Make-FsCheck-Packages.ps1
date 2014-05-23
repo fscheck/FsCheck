4637301ea709667d75710200650e9c3000b2c0e9
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-05-23 23:09:35 +0100

    Remove FsCheck.MSTest package from script

--- Make-FsCheck-Packages.ps1
+++ Make-FsCheck-Packages.ps1
@@ -1,4 +1,3 @@
 nuget pack -sym FsCheck\FsCheck.fsproj -Prop Configuration=Release -Build -Symbols
 nuget pack -sym FsCheck.Xunit\FsCheck.Xunit.fsproj -Prop Configuration=Release -Build -Symbols
 nuget pack -sym FsCheck.NUnit\FsCheck.NUnit.fsproj -Prop Configuration=Release -Build -Symbols
-nuget pack -sym FsCheck.MsTest\FsCheck.MsTest.fsproj -Prop Configuration=Release -Build -Symbols
