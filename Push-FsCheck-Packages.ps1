d6e04d79b69d76d1b1d53776a81f9d5ac48864fc
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-05-24 11:53:52 +0100

    Push 0.9.4

--- Push-FsCheck-Packages.ps1
+++ Push-FsCheck-Packages.ps1
@@ -1,6 +1,5 @@
-
+Nuget update -self
 echo "reminder - have you done:"
-echo "NuGet Update -self (to update NuGet.exe)"
 echo "NuGet SetApiKey Your-API-Key (to make sure you can push)"
 echo "This script will push FsCheck and FsCheck.Xunit to the public NuGet repo."
 $choice = ""
@@ -8,7 +7,7 @@ while ($choice -notmatch "[y|n]"){
     $choice = read-host "Sure? (Y/N)"
     }
 if ($choice -eq "y"){
-    NuGet Push FsCheck.0.9.3.0.nupkg
+    NuGet Push FsCheck.0.9.4.0.nupkg
     NuGet Push FsCheck.Xunit.0.4.1.0.nupkg
     }
     
