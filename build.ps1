a40fa572f10170ad836342674ef77aceaa00909d
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-05-25 10:01:03 +0100

    Add SourceLink.Fake to bootstrap

--- build.ps1
+++ build.ps1
@@ -2,4 +2,7 @@ cls
 If (-Not (Test-Path packages\FAKE\tools\Fake.exe)) {
   .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 }
+If (-Not (Test-Path packages\SourceLink.Fake\tools\SourceLink.fsx)) {
+  .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+}
 packages\FAKE\tools\FAKE.exe build.fsx $args
