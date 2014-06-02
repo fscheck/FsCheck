51a0171442755dc0fd2cfdd7b568ce043838fde9
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-06-02 21:57:58 +0100

    Replace build.ps1 with build.cmd.
    Hopefully this works better with AppVeryor; the ps1 script was not
    passing on the status code of FAKE so the build showed up as success
    even though it failed.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,13 @@
+@echo off
+
 cls
 
 .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 .nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 .nuget\nuget.exe install FSharp.Formatting -OutputDirectory packages -ExcludeVersion
 
-packages\FAKE\tools\FAKE.exe build.fsx $args
+SET TARGET="All"
+
+IF NOT [%1]==[] (set TARGET="%1")
+
+"packages\FAKE\tools\FAKE.exe" "build.fsx" "target=%TARGET%"
