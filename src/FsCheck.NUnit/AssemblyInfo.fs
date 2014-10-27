f5feb7859efd287e54132df4a9e6a48dcaec8264
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-10-27 21:52:36 +0000

    Bump version to 1.0.2

--- src/FsCheck.NUnit/AssemblyInfo.fs
+++ src/FsCheck.NUnit/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsCheck.Nunit")>]
 [<assembly: AssemblyProductAttribute("FsCheck.Nunit")>]
 [<assembly: AssemblyDescriptionAttribute("Integrates FsCheck with NUnit")>]
-[<assembly: AssemblyVersionAttribute("1.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
+[<assembly: AssemblyVersionAttribute("1.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.1"
+    let [<Literal>] Version = "1.0.2"
