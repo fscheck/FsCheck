a5f330e4ea298a08aa8a596816d5834cf1d0b964
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-12-09 21:12:21 +0000

    Bump version to 1.0.4

--- src/FsCheck.NUnit/AssemblyInfo.fs
+++ src/FsCheck.NUnit/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsCheck.Nunit")>]
 [<assembly: AssemblyProductAttribute("FsCheck.Nunit")>]
 [<assembly: AssemblyDescriptionAttribute("Integrates FsCheck with NUnit")>]
-[<assembly: AssemblyVersionAttribute("1.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
+[<assembly: AssemblyVersionAttribute("1.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.3"
+    let [<Literal>] Version = "1.0.4"
--- src/FsCheck.Xunit/AssemblyInfo.fs
+++ src/FsCheck.Xunit/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsCheck.Xunit")>]
 [<assembly: AssemblyProductAttribute("FsCheck.Xunit")>]
 [<assembly: AssemblyDescriptionAttribute("Integrates FsCheck with xUnit.NET")>]
-[<assembly: AssemblyVersionAttribute("1.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
+[<assembly: AssemblyVersionAttribute("1.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.3"
+    let [<Literal>] Version = "1.0.4"
