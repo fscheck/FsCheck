f7f496fab3a78e09c6ed9af66f84ede31a28254e
Răzvan Panda <razvan.panda@gmail.com>
2014-12-17 11:31:32 +0000

    XUnit C# basic examples

---
+++ examples/FsCheck.NUnit.CSharpExamples/FsCheck.XUnit.CSharpExamples/ReverseFixture.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using FsCheck.Xunit;
+
+namespace FsCheck.XUnit.CSharpExamples
+{
+    public class ReverseFixture
+    {
+        [Property]
+        public bool Bcl(int[] xs)
+        {
+            return xs.Reverse().Reverse().SequenceEqual(xs);
+        }
+
+        [Property]
+        public bool ShouldFail_1(int[] xs)
+        {
+            return xs.BadReverse1().SequenceEqual(xs.Reverse());
+        }
+
+        [Property]
+        public bool ShouldFail_2(int[] xs)
+        {
+            return xs.BadReverse2().SequenceEqual(xs.Reverse());
+        }
+
+        [Property(MaxTest = 1000, EndSize = 5)]
+        public bool ShouldFail_3(int[] xs)
+        {
+            return xs.BadReverse3().SequenceEqual(xs.Reverse());
+        }
+    }
+}
