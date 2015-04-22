bb931467ce8d05ff068da2f6f6a02323c80b3ed6
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-04-22 20:35:16 +0100

    Some script changes.

--- tests/FsCheck.Test/Script.fsx
+++ tests/FsCheck.Test/Script.fsx
@@ -2,42 +2,33 @@
 open System.Reflection
 open Microsoft.FSharp.Reflection
 
-let func = (fun ([<param:GenParameterAttribute>] test:int) -> 1)
-
-let funcType = func.GetType()
-
-let invoke = funcType.GetMethod("Invoke")
-
-invoke.GetParameters().[0].GetCustomAttributes(true)
-
-FSharpType.GetFunctionElements funcType |> fst
-
-[<AttributeUsageAttribute(AttributeTargets.Parameter)>]
-type ParameterAttribute(i:int) =
-    inherit Attribute()
-
-
-
-//this works - the attribute is applied to the static member
-let funcy ([<param:ParameterAttribute(1)>] test:int) = 1
-
-//none of the below does anything
-let func2 = (fun ([<param:GenParameterAttribute(1,"1")>] test:int) -> 1)
-let funcNonsense = (fun ([<ICanTypeWhateverIWantHereAndItStillCompiles>] test:int) -> 1)
-
-//applicative
 #r "bin\Debug\FsCheck.dll"
 open FsCheck
 
 //how to generate float between 0 and 5
-let f = 
-    Arb.generate<NormalFloat>
-    |> Gen.map float
-    |> Gen.resize 5
-    |> Gen.map abs
-    |> Gen.suchThat (fun f -> f < 5.)
-    |> Gen.sample 100 10
+//let f = 
+//    Arb.generate<NormalFloat>
+//    |> Gen.map float
+//    |> Gen.resize 5
+//    |> Gen.map abs
+//    |> Gen.suchThat (fun f -> f < 5.)
+//    |> Gen.sample 100 10
 
-let f = fun a b c -> a + b - c
-let ga,gb,gc = Gen.constant 3, Gen.constant 3, Gen.constant 3
-let g = f <!> ga <*> gb <*> gc
+type Arbs =
+  static member SafeString () =
+    Arb.Default.String () |> Arb.filter (fun str -> str <> null && not (str = ""))
+
+  static member NonEmptyStringMaps () =
+    Arb.Default.Map () |> Arb.filter (fun m -> m |> Map.toList |> List.forall (fun (k,v) -> k <> "" && k <> null))
+
+  static member DateTime () =
+    Arb.Default.DateTime ()
+    |> Arb.mapFilter (fun dt -> dt.ToUniversalTime()) (fun dt -> dt.Kind = System.DateTimeKind.Utc)
+
+let fsCheckConfig = { Config.Default with Arbitrary = [ typeof<Arbs> ] }
+
+Check.One(fsCheckConfig,(fun (s:string,m:Map<string,int>) -> not <| String.IsNullOrEmpty s && m |> Map.forall (fun s v -> not <| String.IsNullOrEmpty s)))
+
+Check.One(fsCheckConfig,(fun (s:DateTime) -> s.Kind = DateTimeKind.Utc))
+
+//Gen.sample 10 1 <| Arbs.DateTime().Generator
