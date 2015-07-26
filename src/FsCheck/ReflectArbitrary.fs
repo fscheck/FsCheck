b80992ae675e0b4b4d6c79f13d8b6c992116a252
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-07-26 11:32:05 +0100

    Update URL.

--- src/FsCheck/ReflectArbitrary.fs
+++ src/FsCheck/ReflectArbitrary.fs
@@ -2,7 +2,7 @@
 **  FsCheck                                                                 **
 **  Copyright (c) 2008-2015 Kurt Schelfthout and contributors.              **  
 **  All rights reserved.                                                    **
-**  https://github.com/kurtschelfthout/FsCheck                              **
+**  https://github.com/fscheck/FsCheck                              **
 **                                                                          **
 **  This software is released under the terms of the Revised BSD License.   **
 **  See the file License.txt for the full text.                             **
