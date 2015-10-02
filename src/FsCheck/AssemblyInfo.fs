803e385b8cfec52b3e7b2c7cd4fdb7311d9eb91a
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-10-02 21:11:01 +0100

    Update AssemblyInfo.

--- src/FsCheck/AssemblyInfo.fs
+++ src/FsCheck/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("FsCheck")>]
 [<assembly: AssemblyProductAttribute("FsCheck")>]
 [<assembly: AssemblyDescriptionAttribute("FsCheck is a tool for testing .NET programs automatically using randomly generated test cases.")>]
-[<assembly: AssemblyVersionAttribute("2.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.1.0")>]
+[<assembly: AssemblyVersionAttribute("2.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.1.1")>]
 [<assembly: InternalsVisibleToAttribute("FsCheck.Test")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.1.0"
+    let [<Literal>] Version = "2.1.1"
