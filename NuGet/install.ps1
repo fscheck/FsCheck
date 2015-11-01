655ae8fe010d1efdf586cc450c4aa5ebfa1a73ce
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-11-01 11:22:12 +0000

    Attempt to fix FsCheck.Nunit's install.ps1.

--- NuGet/install.ps1
+++ NuGet/install.ps1
@@ -7,14 +7,7 @@ $version = ""
 # Get-Package -Filter NUnit.Runners also returns packages with NUnit.Runners in the description (e.g. NUnit or SpecFlow.NUnit.Runners)
 # we need to find the one, that we are looking for
 $nunitPackages = Get-Package -Filter NUnit.Runners
-foreach ($nunitPackage in $nunitPackages)
-{
-	# there is a bug in NuGet: the package id for all returned packages is "NUnit.Runners", so filtering that is not enough
-	if ($nunitPackage.Id = "NUnit.Runners" -and $nunitPackage.ProjectUrl.ToString().Contains("nunit")) 
-	{ 
-		$version = $nunitPackage.Version.ToString() 
-	}
-}
+$version = $nunitPackages[0].Version.ToString()
 
 if ($version -ne "") 
 { 
