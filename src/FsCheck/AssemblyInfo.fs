0f79b89b9b864997cf3eae7673407a99f2515622
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-11-05 20:48:28 +0000

    Bump version to 2.2.1

--- src/FsCheck/AssemblyInfo.fs
+++ src/FsCheck/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("FsCheck")>]
 [<assembly: AssemblyProductAttribute("FsCheck")>]
 [<assembly: AssemblyDescriptionAttribute("FsCheck is a tool for testing .NET programs automatically using randomly generated test cases.")>]
-[<assembly: AssemblyVersionAttribute("2.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.2.0")>]
+[<assembly: AssemblyVersionAttribute("2.2.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.2.1")>]
 [<assembly: InternalsVisibleToAttribute("FsCheck.Test")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.2.0"
+    let [<Literal>] Version = "2.2.1"
