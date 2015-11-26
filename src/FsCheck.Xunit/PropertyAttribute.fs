03d2ce4ae9af988fa02c0e0fdae17c10f82c7602
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2015-11-26 20:40:02 +0000

    Lint warnings cleanup take 2.
    Need to leave the ReplayStdGen property in, or the tests fail - I don't
    understand why. :(

--- src/FsCheck.Xunit/PropertyAttribute.fs
+++ src/FsCheck.Xunit/PropertyAttribute.fs
@@ -18,14 +18,14 @@ type PropertyFailedException =
 //can not be an anonymous type because of let mutable.
 type XunitRunner() =
     let mutable result = None
-    member x.Result = result.Value
+    member __.Result = result.Value
     interface IRunner with
-        override x.OnStartFixture t = ()
-        override x.OnArguments (ntest,args, every) =
+        override __.OnStartFixture _ = ()
+        override __.OnArguments (ntest,args, every) =
             printf "%s" (every ntest args)
-        override x.OnShrink(args, everyShrink) =
+        override __.OnShrink(args, everyShrink) =
             printf "%s" (everyShrink args)
-        override x.OnFinished(name,testResult) =
+        override __.OnFinished(_ ,testResult) =
             result <- Some testResult
 
 ///Override Arbitrary instances for FsCheck tests within the attributed class
@@ -34,7 +34,7 @@ type XunitRunner() =
 type ArbitraryAttribute(types:Type[]) =
     inherit Attribute()
     new(typ:Type) = ArbitraryAttribute([|typ|])
-    member x.Arbitrary = types
+    member __.Arbitrary = types
 
 ///Run this method as an FsCheck test.
 [<AttributeUsage(AttributeTargets.Method ||| AttributeTargets.Property, AllowMultiple = false)>]
@@ -51,35 +51,35 @@ type public PropertyAttribute() =
     let mutable arbitrary = Config.Default.Arbitrary |> List.toArray
     ///If set, the seed to use to start testing. Allows reproduction of previous runs. You can just paste
     ///the tuple from the output window, e.g. 12344,12312 or (123,123).
-    member x.Replay with get() = match replay with None -> String.Empty | Some (Random.StdGen (x,y)) -> sprintf "%A" (x,y)
-                    and set(v:string) =
+    member __.Replay with get() = match replay with None -> String.Empty | Some (Random.StdGen (x,y)) -> sprintf "%A" (x,y)
+                     and set(v:string) =
                         //if someone sets this, we want it to throw if it fails
                         let split = v.Trim('(',')').Split([|","|], StringSplitOptions.RemoveEmptyEntries)
                         let elem1 = Int32.Parse(split.[0])
                         let elem2 = Int32.Parse(split.[1])
                         replay <- Some <| Random.StdGen (elem1,elem2)
-    member internal x.ReplayStdGen = replay
+    member internal __.ReplayStdGen = replay //interestingly, although this member is unused, if you remove it tests will fail...?
     ///The maximum number of tests that are run.
-    member x.MaxTest with get() = maxTest and set(v) = maxTest <- v
+    member __.MaxTest with get() = maxTest and set(v) = maxTest <- v
     ///The maximum number of tests where values are rejected, e.g. as the result of ==>
-    member x.MaxFail with get() = maxFail and set(v) = maxFail <- v
+    member __.MaxFail with get() = maxFail and set(v) = maxFail <- v
     ///The size to use for the first test.
-    member x.StartSize with get() = startSize and set(v) = startSize <- v
+    member __.StartSize with get() = startSize and set(v) = startSize <- v
     ///The size to use for the last test, when all the tests are passing. The size increases linearly between Start- and EndSize.
-    member x.EndSize with get() = endSize and set(v) = endSize <- v
+    member __.EndSize with get() = endSize and set(v) = endSize <- v
     ///Output all generated arguments.
-    member x.Verbose with get() = verbose and set(v) = verbose <- v
+    member __.Verbose with get() = verbose and set(v) = verbose <- v
     ///The Arbitrary instances to use for this test method. The Arbitrary instances
     ///are merged in back to front order i.e. instances for the same generated type
     ///at the front of the array will override those at the back.
-    member x.Arbitrary with get() = arbitrary and set(v) = arbitrary <- v
+    member __.Arbitrary with get() = arbitrary and set(v) = arbitrary <- v
     ///If set, suppresses the output from the test if the test is successful. This can be useful when running tests
     ///with TestDriven.net, because TestDriven.net pops up the Output window in Visual Studio if a test fails; thus,
     ///when conditioned to that behaviour, it's always a bit jarring to receive output from passing tests.
     ///The default is false, which means that FsCheck will also output test results on success, but if set to true,
     ///FsCheck will suppress output in the case of a passing test. This setting doesn't affect the behaviour in case of
     ///test failures.
-    member x.QuietOnSuccess with get() = quietOnSuccess and set(v) = quietOnSuccess <- v
+    member __.QuietOnSuccess with get() = quietOnSuccess and set(v) = quietOnSuccess <- v
 
 /// The xUnit2 test runner for the PropertyAttribute that executes the test via FsCheck
 type PropertyTestCase(diagnosticMessageSink:IMessageSink, defaultMethodDisplay:TestMethodDisplay, testMethod:ITestMethod, ?testMethodArguments:obj []) =
@@ -139,14 +139,14 @@ type PropertyTestCase(diagnosticMessageSink:IMessageSink, defaultMethodDisplay:T
                           | TestResult.True _ ->
                             let output = Runner.onFinishedToString "" xunitRunner.Result
                             new TestPassed(test, timer.Total, output) :> TestResultMessage
-                          | TestResult.Exhausted testdata ->
+                          | TestResult.Exhausted _ ->
                             summary.Failed <- summary.Failed + 1
                             upcast new TestFailed(test, timer.Total, "", new PropertyFailedException(xunitRunner.Result))
                           | TestResult.False (testdata, originalArgs, shrunkArgs, Outcome.Exception e, seed)  ->
                             summary.Failed <- summary.Failed + 1
                             let message = sprintf "%s%s" Environment.NewLine (Runner.onFailureToString "" testdata originalArgs shrunkArgs seed)
                             upcast new TestFailed(test, timer.Total, "", new PropertyFailedException(message, e))
-                          | TestResult.False (testdata, originalArgs, shrunkArgs, outcome, seed)  ->
+                          | TestResult.False _ ->
                             summary.Failed <- summary.Failed + 1
                             upcast new TestFailed(test, timer.Total, "", new PropertyFailedException(xunitRunner.Result))
                 with
@@ -177,9 +177,9 @@ type PropertyDiscoverer(messageSink:IMessageSink) =
 
     new () = PropertyDiscoverer(null)
 
-    member x.MessageSink = messageSink
+    member __.MessageSink = messageSink
 
     interface IXunitTestCaseDiscoverer with
-        override this.Discover(discoveryOptions:ITestFrameworkDiscoveryOptions, testMethod:ITestMethod, factAttribute:IAttributeInfo)=
+        override this.Discover(discoveryOptions:ITestFrameworkDiscoveryOptions, testMethod:ITestMethod, _:IAttributeInfo)=
             let ptc = new PropertyTestCase(this.MessageSink, discoveryOptions.MethodDisplayOrDefault(), testMethod)
             Seq.singleton (ptc :> IXunitTestCase)
