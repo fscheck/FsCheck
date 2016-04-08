303519c727cb0483cd6419c00eb6ea86d458c908
Enrico Sada <enrico@sada.io>
2016-04-08 19:03:45 +0200

    add test of library

--- src/FsCheck.Dotnetcli.Tests/Program.fs
+++ src/FsCheck.Dotnetcli.Tests/Program.fs
@@ -2,8 +2,15 @@
 
 open System
 
+open FsCheck
+
 [<EntryPoint>]
 let main argv = 
     printfn "Hello World!"
     printfn "%A" argv
+    
+    let revRevIsOrig (xs:list<int>) = List.rev(List.rev xs) = xs
+    
+    Check.Quick revRevIsOrig
+    
     0 // return an integer exit code
