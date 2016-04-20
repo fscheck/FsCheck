74adef5f71c96f6e1588b2ded2ee05df5150e768
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2016-04-20 21:00:46 +0100

    Update AssemblyInfos.

--- src/FsCheck.Xunit/AssemblyInfo.fs
+++ src/FsCheck.Xunit/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsCheck.Xunit")>]
 [<assembly: AssemblyProductAttribute("FsCheck.Xunit")>]
 [<assembly: AssemblyDescriptionAttribute("Integrates FsCheck with xUnit.NET")>]
-[<assembly: AssemblyVersionAttribute("2.3.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.3.1")>]
+[<assembly: AssemblyVersionAttribute("2.4.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.4.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.3.1"
-    let [<Literal>] InformationalVersion = "2.3.1"
+    let [<Literal>] Version = "2.4.0"
+    let [<Literal>] InformationalVersion = "2.4.0"
