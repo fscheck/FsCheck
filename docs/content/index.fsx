098d7a106efc1f5d1f017c76fc8a3ff2d9049562
M Hickford <mirth.hickford@gmail.com>
2017-08-12 19:24:05 +0100

    Add introduction to homepage

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -7,6 +7,8 @@
 FsCheck
 =======
 
+FsCheck is a tool for testing .NET programs automatically. The programmer provides a specification of the program, in the form of properties which functions, methods or objects should satisfy, and FsCheck then tests that the properties hold in a large number of randomly generated cases. While writing the properties, you are actually writing a testable specification of your program. Specifications are expressed in F#, C# or VB, using combinators defined in the FsCheck library. FsCheck provides combinators to define properties, observe the distribution of test data, and define test data generators. When a property fails, FsCheck automatically displays a minimal counter example.
+
 <div class="row">
   <div class="span1"></div>
   <div class="span6">
