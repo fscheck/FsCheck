65c15ca23ea3d81652916e55e33cbb3a8e530f3a
Michal Niegrzybowski <micnie@jetshop.se>
2018-05-10 07:18:45 +0200

    remove not needed special case

--- src/FsCheck/Gen.fs
+++ src/FsCheck/Gen.fs
@@ -533,8 +533,7 @@ module Gen =
     let subListOf l =
         let elems = Array.ofSeq l
         gen {// Generate indices into the array (up to the number of elements)
-             let maxNumberOfElements = if elems.Length = 1 then 1 else elems.Length-1
-             let! size = choose(0, maxNumberOfElements)
+             let! size = choose(0, elems.Length)
              let! indices = listOfLength size (choose(0, elems.Length-1)) 
              let subSeq = indices |> Seq.distinct |> Seq.map (fun i -> elems.[i])
              return List.ofSeq subSeq }
