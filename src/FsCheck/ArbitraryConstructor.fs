3c9c70d1068fcd42adbf3584fe886d2f074d1e71
Nazar Vinnichuk <kharacternyk@gmail.com>
2019-03-16 20:28:53 +0200

    Add ArbitraryConstructor module

---
+++ src/FsCheck/ArbitraryConstructor.fs
@@ -0,0 +1,36 @@
+﻿namespace FsCheck.Experimental
+
+open System.Reflection
+open System
+open FsCheck
+
+//Is not record to easily add new params
+///Represents constraints for generated type
+type TypeConstraints(origin : Assembly seq, 
+                     ?requiredAttributes : Type seq,
+                     ?requiredParents : Type seq) =
+    member __.Origin = origin
+    member __.RequiredAttributes = defaultArg requiredAttributes Seq.empty
+    member __.RequiredParents = defaultArg requiredParents Seq.empty
+
+
+module ArbitraryConstructor =
+
+#if NETSTANDARD2_0 || NET452 //Attribute.IsDefined is not available in lower versions, fix later 
+    let typeFrom (typeConstraints : TypeConstraints) =
+        let origin = typeConstraints.Origin
+        let allTypes = 
+            origin
+            |> Seq.map (fun assembly -> assembly.GetExportedTypes() |> Seq.ofArray)
+            |> Seq.concat
+        let withAttributes =
+            let hasAttribute (t : Type) (a : Type) =
+                Attribute.IsDefined(t, a)
+            allTypes |> Seq.filter (fun t -> Seq.forall (hasAttribute t) typeConstraints.RequiredAttributes)
+        let withParents =
+            let isDerived (t : Type) (b : Type) =
+                b.IsAssignableFrom t
+            withAttributes |> Seq.filter (fun t -> Seq.forall (isDerived t) typeConstraints.RequiredParents)
+        withParents
+#endif        
+     
---
+++ tests/FsCheck.Test/ArbitraryConstructor.fs
@@ -0,0 +1,23 @@
+﻿namespace FsCheck.Experimental.Test
+
+module ArbitraryConstructor =
+
+    open Xunit
+    open FsCheck
+    open FsCheck.Xunit
+    open System
+    open FsCheck.Experimental
+    open FsCheck.Experimental.ArbitraryConstructor
+
+    [<Fact>]
+    let ``Can retrieve types``() =
+        let assembly = typeof<string>.Assembly
+        let constr = TypeConstraints(assembly |> Seq.singleton)
+        typeFrom constr |> ignore
+    
+    [<Property(MaxTest = 1)>]
+    let ``Object parent equals to usnspecified parent``() =
+        let assembly = typeof<string>.Assembly
+        let constrA = TypeConstraints(assembly |> Seq.singleton, requiredParents = [typeof<obj>])
+        let constrB = TypeConstraints(assembly |> Seq.singleton, requiredParents = [])
+        (typeFrom constrA |> Seq.length) = (typeFrom constrB |> Seq.length)
