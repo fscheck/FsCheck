2cc6c70bba7e0963c72e689562d26470697ae37b
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2020-02-09 15:12:14 +0000

    Build and deploy tooling update (#504)
    Use dotnet tool to bootstrap Fake.
    Update build group to latest version (mainly fake and fsformatting).
    Fix some Fake deprecation warnings.
    Fix some build.fsx bugs.
    Update and simplify build.sh and build.cmd.
    Fix documentation fsx scripts - they run using netstandard2.0 assemblies now.
    Update AppVeyor and travis config.
    Use .NET Core to build everything, including on *nix, targeting other frameworks. Add reference assemblies to be able to buld on nix, removing a mono dependency.
    Convert a few example projects to netcoreapp3.1.

--- build.cmd
+++ build.cmd
@@ -1,14 +1,6 @@
 @echo off
 
 cls
-
-.paket\paket.exe restore
-if errorlevel 1 (
-exit /b %errorlevel%
-)
-
-SET TARGET="RunTests"
-
-IF NOT [%1]==[] (set TARGET="%1")
-
-"packages\build\FAKE\tools\FAKE.exe" "build.fsx" "target=%TARGET%"
+dotnet --info
+dotnet tool restore
+dotnet fake run build.fsx %*
