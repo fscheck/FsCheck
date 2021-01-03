92df670fea7b152604fe7f86cb4dbe8ddc5ef17f
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2021-01-03 11:37:27 +0000

    Remove all NETSTANDARD compile time constants.

--- src/FsCheck.NUnit/FsCheckPropertyAttribute.fs
+++ src/FsCheck.NUnit/FsCheckPropertyAttribute.fs
@@ -142,10 +142,7 @@ and FsCheckTestMethod(mi : IMethodInfo, parentSuite : Test) =
 
     member private x.HandleException ex testResult failureSite =
         match ex with
-#if NETSTANDARD1_6
-#else
         | :? ThreadAbortException -> Thread.ResetAbort()
-#endif
         | _ -> ()
         testResult.RecordException(x.FilterException <| ex, failureSite)
 
