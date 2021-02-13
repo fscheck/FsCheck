0ea3da0e50ecea447c4e110ea67cfa474fda54ad
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2021-02-13 11:17:45 +0000

    Split Prop into FSharp.Prop and Fluent.Prop.

--- src/FsCheck/Fluent.Prop.fs
+++ src/FsCheck/Fluent.Prop.fs
@@ -1,11 +1,58 @@
-﻿namespace FsCheck
+﻿namespace FsCheck.Fluent
 
 open System
 open System.Runtime.CompilerServices
 
+open FsCheck
+open FsCheck.FSharp
+open Testable
+
 ///Extensions to construct Properties.
-[<Extension>]
-type PropertyExtensions =
+[<Extension;Sealed;AbstractClass>]
+type Prop private() =
+
+    static member ForAll(arb:Arbitrary<'Value>, body:Action<'Value>) = Prop.forAll arb body.Invoke
+
+    static member ForAll(arb:Arbitrary<'Value>, body:Func<'Value,bool>) = Prop.forAll arb body.Invoke
+
+    static member ForAll(arb:Arbitrary<'Value>, body:Func<'Value,Threading.Tasks.Task<bool>>) = forAll arb body.Invoke
+
+    static member ForAll(arb:Arbitrary<'Value>, body:Func<'Value,Property>) = forAll arb body.Invoke
+
+    static member ForAll(body:Action<'Value>) = property body.Invoke
+
+    static member ForAll(body:Func<'Value,bool>) = property body.Invoke
+
+    static member ForAll(body:Func<'Value,Threading.Tasks.Task<bool>>) = property body.Invoke
+
+    static member ForAll(body:Func<'Value,Property>) = property body.Invoke
+
+    static member ForAll(arb1:Arbitrary<'V1>,arb2:Arbitrary<'V2>, body:Action<'V1,'V2>) = forAll arb1 (fun v1 -> forAll arb2 (fun v2 -> body.Invoke(v1,v2)))
+
+    static member ForAll(arb1:Arbitrary<'V1>,arb2:Arbitrary<'V2>, body:Func<'V1,'V2,bool>) = forAll arb1 (fun v1 -> forAll arb2 (fun v2 -> body.Invoke(v1,v2)))
+
+    static member ForAll(arb1:Arbitrary<'V1>,arb2:Arbitrary<'V2>, body:Func<'V1,'V2,Property>) = forAll arb1 (fun v1 -> forAll arb2 (fun v2 -> body.Invoke(v1,v2)))
+
+    static member ForAll(body:Action<'V1,'V2>) = property body.Invoke
+
+    static member ForAll(body:Func<'V1,'V2,bool>) = property body.Invoke
+
+    static member ForAll(body:Func<'V1,'V2,Property>) = property body.Invoke
+    
+    static member ForAll(arb1:Arbitrary<'V1>,arb2:Arbitrary<'V2>,arb3:Arbitrary<'V3>, body:Action<'V1,'V2,'V3>) = 
+        forAll arb1 (fun v1 -> forAll arb2 (fun v2 -> forAll arb3 (fun v3 -> body.Invoke(v1,v2,v3))))
+
+    static member ForAll(arb1:Arbitrary<'V1>,arb2:Arbitrary<'V2>,arb3:Arbitrary<'V3>, body:Func<'V1,'V2,'V3,bool>) = 
+        forAll arb1 (fun v1 -> forAll arb2 (fun v2 -> forAll arb3 (fun v3 -> body.Invoke(v1,v2,v3))))
+
+    static member ForAll(arb1:Arbitrary<'V1>,arb2:Arbitrary<'V2>,arb3:Arbitrary<'V3>, body:Func<'V1,'V2,'V3,Property>) = 
+        forAll arb1 (fun v1 -> forAll arb2 (fun v2 -> forAll arb3 (fun v3 -> body.Invoke(v1,v2,v3))))
+
+    static member ForAll(body:Action<'V1,'V2,'V3>) = property body.Invoke
+
+    static member ForAll(body:Func<'V1,'V2,'V3,bool>) = property body.Invoke
+
+    static member ForAll(body:Func<'V1,'V2,'V3,Property>) = property body.Invoke
 
     /// Turns a testable type into a property.
     [<Extension>]
@@ -33,57 +80,68 @@ type PropertyExtensions =
     [<Extension>]
     static member When(property:Action, condition) = 
         if isNull property then nullArg "property"
-        Prop.given condition (PropertyExtensions.ToProperty property, Prop.ofTestable Res.rejected)
+        Prop.given condition (Prop.ToProperty property, Prop.ofTestable Res.rejected)
 
     ///Conditional property combinator. Resulting property holds if the property holds when the condition does.
     [<Extension>]
     static member When(property:Func<bool>, condition) = 
         if isNull property then nullArg "property"
-        Prop.given condition (PropertyExtensions.ToProperty property, Prop.ofTestable Res.rejected)
+        Prop.given condition (Prop.ToProperty property, Prop.ofTestable Res.rejected)
 
     ///Conditional property combinator. Resulting property holds if the property holds when the condition does.
     [<Extension>]
     static member When(property:Property, condition) = 
         Prop.given condition (property,Prop.ofTestable Res.rejected)
 
+    ///Conditional property combinator. Resulting property holds if the given property holds whenever the condition does. See also operator:  'assertion ==> property'
+    [<Extension>]
+    static member When(condition, assertion : Func<'Testable>) =
+        if isNull assertion then nullArg "assertion"
+        Prop.filter condition (fun () -> assertion.Invoke ())
+
+    ///Conditional property combinator. Resulting property holds if the given property holds whenever the condition does. See also operator:  'assertion ==> property'
+    static member When(condition, assertion : Action) =
+        if isNull assertion then nullArg "assertion"
+        Prop.filter condition (fun () -> assertion.Invoke ())
+
     ///Conditional property combinator. Resulting property holds if the property holds when the condition does.
     [<Extension>]
-    static member Implies(condition, property:bool) = 
-        PropertyExtensions.When(property, condition)
+    static member Implies(condition: bool, property:bool) = 
+        Prop.When(property, condition)
 
     ///Conditional property combinator. Resulting property holds if the property holds when the condition does.
     [<Extension>]
     static member Implies(condition, property:Action) = 
         if isNull property then nullArg "property"
-        PropertyExtensions.When(property, condition)
+        Prop.When(property, condition)
 
     ///Conditional property combinator. Resulting property holds if the property holds when the condition does.
     [<Extension>]
     static member Implies(condition, property:Func<bool>) = 
         if isNull property then nullArg "property"
-        PropertyExtensions.When(property, condition)
+        Prop.When(property, condition)
 
     ///Conditional property combinator. Resulting property holds if the property holds when the condition does.
     [<Extension>]
     static member Implies(condition, property:Property) = 
-        PropertyExtensions.When(property, condition)
+        Prop.When(property, condition)
 
     //Classify test cases. Test cases satisfying the condition are assigned the classification given.
     [<Extension>]
     static member Classify (property: Action, cond:bool, name:string) = 
         if isNull property then nullArg "property"
-        Prop.classify cond name (PropertyExtensions.ToProperty(property))
+        Prop.classify cond name (Prop.ToProperty(property))
 
     ///Classify test cases. Test cases satisfying the condition are assigned the classification given.
     [<Extension>]
     static member Classify (property: bool, cond:bool, name:string) = 
-        Prop.classify cond name (PropertyExtensions.ToProperty(property))
+        Prop.classify cond name (Prop.ToProperty(property))
 
     ///Classify test cases. Test cases satisfying the condition are assigned the classification given.
     [<Extension>]
     static member Classify (property: Func<bool>, cond:bool, name:string) = 
         if isNull property then nullArg "property"
-        Prop.classify cond name (PropertyExtensions.ToProperty(property))
+        Prop.classify cond name (Prop.ToProperty(property))
 
     ///Classify test cases. Test cases satisfying the condition are assigned the classification given.
     [<Extension>]
@@ -95,18 +153,18 @@ type PropertyExtensions =
     [<Extension>]
     static member Trivial(property:Action, condition) =
         if isNull property then nullArg "property"
-        Prop.trivial condition (PropertyExtensions.ToProperty(property))
+        Prop.trivial condition (Prop.ToProperty(property))
 
     ///Count trivial cases. Test cases for which the condition is True are classified as trivial.
     [<Extension>]
     static member Trivial(property:bool, condition) =
-        Prop.trivial condition (PropertyExtensions.ToProperty(property))
+        Prop.trivial condition (Prop.ToProperty(property))
 
     ///Count trivial cases. Test cases for which the condition is True are classified as trivial.
     [<Extension>]
     static member Trivial(property:Func<bool>, condition) =
         if isNull property then nullArg "property"
-        Prop.trivial condition (PropertyExtensions.ToProperty(property))
+        Prop.trivial condition (Prop.ToProperty(property))
 
     ///Count trivial cases. Test cases for which the condition is True are classified as trivial.
     [<Extension>]
@@ -118,20 +176,20 @@ type PropertyExtensions =
     [<Extension>]
     static member Collect (property: Action, v:'CollectedValue) = 
         if isNull property then nullArg "property"
-        Prop.collect v (PropertyExtensions.ToProperty(property))
+        Prop.collect v (Prop.ToProperty(property))
 
     ///Collect data values. The argument of collect is evaluated in each test case, 
     ///and the distribution of values is reported.
     [<Extension>]
     static member Collect (property: bool, v:'CollectedValue) = 
-        Prop.collect v (PropertyExtensions.ToProperty(property))
+        Prop.collect v (Prop.ToProperty(property))
 
     ///Collect data values. The argument of collect is evaluated in each test case, 
     ///and the distribution of values is reported.
     [<Extension>]
     static member Collect (property: Func<bool>, v:'CollectedValue) = 
         if isNull property then nullArg "property"
-        Prop.collect v (PropertyExtensions.ToProperty(property))
+        Prop.collect v (Prop.ToProperty(property))
 
     ///Collect data values. The argument of collect is evaluated in each test case, 
     ///and the distribution of values is reported.
@@ -143,63 +201,65 @@ type PropertyExtensions =
     [<Extension>]
     static member Label(property: Action, label) =
         if isNull property then nullArg "property"
-        Prop.label label (PropertyExtensions.ToProperty(property))
+        Prop.label label (Prop.ToProperty(property))
 
     ///Add the given label to the property. The labels of a failing sub-property are displayed when it fails.
     [<Extension>]
     static member Label(property: bool, label) =
-        Prop.label label (PropertyExtensions.ToProperty(property))
+        Prop.label label (Prop.ToProperty(property))
 
     ///Add the given label to the property. The labels of a failing sub-property are displayed when it fails.
     [<Extension>]
     static member Label(property: Func<bool>, label) =
         if isNull property then nullArg "property"
-        Prop.label label (PropertyExtensions.ToProperty(property))
+        Prop.label label (Prop.ToProperty(property))
 
     ///Add the given label to the property. The labels of a failing sub-property are displayed when it fails.
     [<Extension>]
     static member Label(property: Property, label) =
         Prop.label label property
 
+    static member Discard() = Prop.discard()
+
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: bool, right:Action) =
         if isNull right then nullArg "right"
-        (PropertyExtensions.ToProperty left) .&. (PropertyExtensions.ToProperty right)
+        (Prop.ToProperty left) .&. (Prop.ToProperty right)
         
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: bool, right:bool) =
-        (PropertyExtensions.ToProperty left) .&. (PropertyExtensions.ToProperty right)
+        (Prop.ToProperty left) .&. (Prop.ToProperty right)
 
 
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: bool, right:Func<bool>) =
         if isNull right then nullArg "right"
-        (PropertyExtensions.ToProperty left) .&. (PropertyExtensions.ToProperty right)
+        (Prop.ToProperty left) .&. (Prop.ToProperty right)
 
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: bool, right:Property) =
-        (PropertyExtensions.ToProperty left) .&. right
+        (Prop.ToProperty left) .&. right
 
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: Property, right:Action) =
         if isNull right then nullArg "right"
-        left .&. (PropertyExtensions.ToProperty right)
+        left .&. (Prop.ToProperty right)
 
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: Property, right:bool) =
-        left .&. (PropertyExtensions.ToProperty right)
+        left .&. (Prop.ToProperty right)
 
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
     static member And(left: Property, right:Func<bool>) =
         if isNull right then nullArg "right"
-        left .&. (PropertyExtensions.ToProperty right)
+        left .&. (Prop.ToProperty right)
 
     ///Construct a property that succeeds if both succeed. (cfr 'and')
     [<Extension>]
@@ -210,40 +270,40 @@ type PropertyExtensions =
     [<Extension>]
     static member Or(left: bool, right:Action) =
         if isNull right then nullArg "right"
-        (PropertyExtensions.ToProperty left) .|. (PropertyExtensions.ToProperty right)
+        (Prop.ToProperty left) .|. (Prop.ToProperty right)
         
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
     static member Or(left: bool, right:bool) =
-        (PropertyExtensions.ToProperty left) .|. (PropertyExtensions.ToProperty right)
+        (Prop.ToProperty left) .|. (Prop.ToProperty right)
 
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
     static member Or(left: bool, right:Func<bool>) =
         if isNull right then nullArg "right"
-        (PropertyExtensions.ToProperty left) .|. (PropertyExtensions.ToProperty right)
+        (Prop.ToProperty left) .|. (Prop.ToProperty right)
 
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
     static member Or(left: bool, right:Property) =
-        (PropertyExtensions.ToProperty left) .|. right
+        (Prop.ToProperty left) .|. right
 
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
     static member Or(left: Property, right:Action) =
         if isNull right then nullArg "right"
-        left .|. (PropertyExtensions.ToProperty right)
+        left .|. (Prop.ToProperty right)
 
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
     static member Or(left: Property, right:bool) =
-        left .|. (PropertyExtensions.ToProperty right)
+        left .|. (Prop.ToProperty right)
 
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
     static member Or(left: Property, right:Func<bool>) =
         if isNull right then nullArg "right"
-        left .|. (PropertyExtensions.ToProperty right)
+        left .|. (Prop.ToProperty right)
 
     ///Construct a property that fails if both fail. (cfr 'or')
     [<Extension>]
