b5d7b53df6ea3f6ec5b24f3f96975bdabd31e523
Laurence King <laurenceking@hotmail.co.uk>
2021-04-05 21:50:41 +0100

    Allow xunit Properties attribute to be used on assemblies as well as classes

--- docs/RunningTests.fsx
+++ docs/RunningTests.fsx
@@ -148,23 +148,39 @@ instance just for that test method. You can also use the `PropertiesAttribute` (
 type Positive =
     static member Double() =
         Arb.Default.Float()
-        |> Arb.mapFilter abs (fun t -> t >= 0.0)
+        |> Arb.mapFilter abs (fun t -> t > 0.0)
 
 type Negative =
     static member Double() =
         Arb.Default.Float()
-        |> Arb.mapFilter (abs >> ((-) 0.0)) (fun t -> t <= 0.0)
+        |> Arb.mapFilter (abs >> ((-) 0.0)) (fun t -> t < 0.0)
+
+type Zero =
+    static member Double() =
+        0.0
+        |> Gen.constant
+        |> Arb.fromGen
+
+[<assembly: Properties( Arbitrary = [| typeof<Zero> |])>] do()
+module ModuleWithoutProperties =
+    [<Property>]
+    let ``should use Arb instances from enclosing module``(underTest:float) =
+        underTest = 0.0
+
+    [<Property( Arbitrary=[| typeof<Positive> |] )>]
+    let ``should use Arb instance on method``(underTest:float) =
+        underTest > 0.0
 
 [<Properties( Arbitrary=[| typeof<Negative> |] )>]
 module ModuleWithProperties =
 
     [<Property>]
     let ``should use Arb instances from enclosing module``(underTest:float) =
-        underTest <= 0.0
+        underTest < 0.0
 
     [<Property( Arbitrary=[| typeof<Positive> |] )>]
     let ``should use Arb instance on method``(underTest:float) =
-        underTest >= 0.0
+        underTest > 0.0
 
 (**
 Using `PropertiesAttribute` and `PropertyAttribute` you can set any configuration. For example in following module:
