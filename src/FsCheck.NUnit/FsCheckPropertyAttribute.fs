231a6a508092c9257e757307ce45c315bd43db02
Oleksandr Bogomaz <alex_bogomaz@ukr.net>
2024-12-27 20:32:33 +0000

    implement PropertiesAttribute for NUnit (#689)
    - resolves #586

--- src/FsCheck.NUnit/FsCheckPropertyAttribute.fs
+++ src/FsCheck.NUnit/FsCheckPropertyAttribute.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Threading
+open System.Reflection
 
 open FsCheck
 
@@ -29,48 +30,132 @@ type private NunitRunner() =
         override __.OnFinished(_,testResult) =
             result <- Some testResult
 
+type internal PropertyConfig =
+    { MaxTest        : Option<int>
+      MaxRejected    : Option<int>
+      Replay         : Option<string>
+      Parallelism    : Option<int>
+      StartSize      : Option<int>
+      EndSize        : Option<int>
+      Verbose        : Option<bool>
+      QuietOnSuccess : Option<bool>
+      Arbitrary      : Type[] }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module internal PropertyConfig = 
+    let orElse y = function
+        | Some x -> Some x
+        | None   -> y
+
+    let orDefault x y = defaultArg y x
+
+    let zero =
+        { MaxTest        = None
+          MaxRejected    = None
+          Replay         = None
+          Parallelism    = None
+          StartSize      = None
+          EndSize        = None
+          Verbose        = None
+          QuietOnSuccess = None
+          Arbitrary      = [||] }
+
+    let combine extra original =
+        { MaxTest        = extra.MaxTest        |> orElse original.MaxTest
+          MaxRejected    = extra.MaxRejected    |> orElse original.MaxRejected
+          Replay         = extra.Replay         |> orElse original.Replay
+          Parallelism    = extra.Parallelism    |> orElse original.Parallelism
+          StartSize      = extra.StartSize      |> orElse original.StartSize
+          EndSize        = extra.EndSize        |> orElse original.EndSize
+          Verbose        = extra.Verbose        |> orElse original.Verbose
+          QuietOnSuccess = extra.QuietOnSuccess |> orElse original.QuietOnSuccess
+          Arbitrary      = Array.append extra.Arbitrary original.Arbitrary }
+
+    let parseReplay (str: string) =
+        //if someone sets this, we want it to throw if it fails
+        let split = str.Trim('(',')').Split([|","|], StringSplitOptions.RemoveEmptyEntries)
+        let seed = UInt64.Parse(split.[0])
+        let gamma = UInt64.Parse(split.[1])
+        let size = if split.Length = 3 then Some <| Convert.ToInt32(UInt32.Parse(split.[2])) else None
+        { Rnd = Rnd (seed,gamma); Size = size }
+
+    let toConfig runner propertyConfig =
+        Config.Default
+              .WithReplay(
+                  propertyConfig.Replay
+                  |> Option.map parseReplay
+                  |> orElse Config.Default.Replay
+              )
+              .WithParallelRunConfig(
+                  propertyConfig.Parallelism
+                  |> Option.map (fun i -> { MaxDegreeOfParallelism = i })
+                  |> orElse Config.Default.ParallelRunConfig
+              )
+              .WithMaxTest(propertyConfig.MaxTest |> orDefault Config.Default.MaxTest)
+              .WithMaxRejected(propertyConfig.MaxRejected |> orDefault Config.Default.MaxRejected)
+              .WithStartSize(propertyConfig.StartSize |> orDefault Config.Default.StartSize)
+              .WithEndSize(propertyConfig.EndSize |> orDefault Config.Default.EndSize)
+              .WithQuietOnSuccess(propertyConfig.QuietOnSuccess |> orDefault Config.Default.QuietOnSuccess)
+              .WithArbitrary(Seq.toList propertyConfig.Arbitrary)
+              .WithRunner(runner)
+              .WithEvery(
+                  if propertyConfig.Verbose |> Option.exists id then 
+                      Config.Verbose.Every
+                  else
+                      Config.Quick.Every
+              )
+              .WithEveryShrink(
+                  if propertyConfig.Verbose |> Option.exists id then 
+                      Config.Verbose.EveryShrink
+                  else 
+                      Config.Quick.EveryShrink
+              )
+
 ///Run this method as an FsCheck test.
 [<AttributeUsage(AttributeTargets.Method, AllowMultiple = false)>]
 type PropertyAttribute() =
     inherit TestAttribute()
 
-    let mutable maxTest = Config.Default.MaxTest
-    let mutable maxFail = Config.Default.MaxRejected
-    let mutable startSize = Config.Default.StartSize
-    let mutable endSize = Config.Default.EndSize
-    let mutable verbose = false
-    let mutable quietOnSuccess = false
+    let mutable config = PropertyConfig.zero
     let mutable replay = null
     let mutable parallelism = -1
-    let mutable arbitrary = Array.empty
+    let mutable maxTest = -1
+    let mutable maxFail = -1
+    let mutable startSize = -1
+    let mutable endSize = -1
+    let mutable verbose = false
+    let mutable arbitrary = [||]
+    let mutable quietOnSuccess = false
 
     ///If set, the seed to use to start testing. Allows reproduction of previous runs. You can just paste
     ///the tuple from the output window, e.g. 12344,12312 or (123,123).
-    member __.Replay with get() = replay and set(v) = replay <- v
+    member __.Replay with get() = replay and set(v) = replay <- v; config <- {config with Replay = if String.IsNullOrEmpty v then None else Some v}
     ///If set, run tests in parallel. Useful for Task/async related work and heavy number crunching
     ///Environment.ProcessorCount have been found to be useful default.
-    member __.Parallelism with get() = parallelism and set(v) = parallelism <- v
+    member __.Parallelism with get() = parallelism and set(v) = parallelism <- v; config <- {config with Parallelism = Some v}
     ///The maximum number of tests that are run.
-    member __.MaxTest with get() = maxTest and set(v) = maxTest <- v
+    member __.MaxTest with get() = maxTest and set(v) = maxTest <- v; config <- {config with MaxTest = Some v}
     ///The maximum number of tests where values are rejected, e.g. as the result of ==>
-    member __.MaxFail with get() = maxFail and set(v) = maxFail <- v
+    member __.MaxFail with get() = maxFail and set(v) = maxFail <- v; config <- {config with MaxRejected = Some v}
     ///The size to use for the first test.
-    member __.StartSize with get() = startSize and set(v) = startSize <- v
+    member __.StartSize with get() = startSize and set(v) = startSize <- v; config <- {config with StartSize = Some v}
     ///The size to use for the last test, when all the tests are passing. The size increases linearly between Start- and EndSize.
-    member __.EndSize with get() = endSize and set(v) = endSize <- v
+    member __.EndSize with get() = endSize and set(v) = endSize <- v; config <- {config with EndSize = Some v}
     ///Output all generated arguments.
-    member __.Verbose with get() = verbose and set(v) = verbose <- v
+    member __.Verbose with get() = verbose and set(v) = verbose <- v; config <- {config with Verbose = Some v}
     ///The Arbitrary instances to use for this test method. The Arbitrary instances
     ///are merged in back to front order i.e. instances for the same generated type
     ///at the front of the array will override those at the back.
-    member __.Arbitrary with get() = arbitrary and set(v) = arbitrary <- v
+    member __.Arbitrary with get() = arbitrary and set(v) = arbitrary <- v; config <- {config with Arbitrary = v}
     ///If set, suppresses the output from the test if the test is successful. This can be useful when running tests
     ///with TestDriven.net, because TestDriven.net pops up the Output window in Visual Studio if a test fails; thus,
     ///when conditioned to that behaviour, it's always a bit jarring to receive output from passing tests.
     ///The default is false, which means that FsCheck will also output test results on success, but if set to true,
     ///FsCheck will suppress output in the case of a passing test. This setting doesn't affect the behaviour in case of
     ///test failures.
-    member __.QuietOnSuccess with get() = quietOnSuccess and set(v) = quietOnSuccess <- v
+    member __.QuietOnSuccess with get() = quietOnSuccess and set(v) = quietOnSuccess <- v; config <- {config with QuietOnSuccess = Some v}
+
+    member internal __.Config = config
 
     interface ISimpleTestBuilder with
         override __.BuildFrom(mi, suite) =
@@ -90,6 +175,15 @@ type PropertyAttribute() =
                 override __.Execute context = match command.Test with
                                               | :? FsCheckTestMethod as testMethod -> testMethod.RunTest(context)
                                               | _ -> command.Execute(context) }
+
+and
+    ///Set common configuration for all properties within this class or module
+    [<AttributeUsage(AttributeTargets.Class ||| AttributeTargets.Assembly, AllowMultiple = false)>]
+    PropertiesAttribute() =
+    inherit PropertyAttribute()
+
+        interface IApplyToTest with
+            override __.ApplyToTest(test) = ()
 
 and FsCheckTestMethod(mi : IMethodInfo, parentSuite : Test) =
     inherit TestMethod(mi, parentSuite)
@@ -140,6 +234,30 @@ and FsCheckTestMethod(mi : IMethodInfo, parentSuite : Test) =
         x.Method.GetCustomAttributes<PropertyAttribute> false
         |> Seq.head 
 
+    member private x.Init(runner) =
+        let combineAttributes (configs: (PropertyConfig option) list) =
+            configs
+            |> List.choose id
+            |> List.reduce(fun higherLevelAttribute lowerLevelAttribute ->
+                PropertyConfig.combine lowerLevelAttribute higherLevelAttribute)
+
+        let getPropertiesOnDeclaringClasses (testClass: ITypeInfo) =
+            [   let mutable current: Type = testClass.Type
+                while not (isNull current) do
+                    yield current.GetTypeInfo().GetCustomAttributes<PropertiesAttribute>()
+                          |> Seq.tryHead
+                          |> Option.map (fun attr -> attr.Config)
+                    current <- current.DeclaringType]
+            |> List.rev
+
+        let config = combineAttributes [
+              yield x.Method.TypeInfo.Assembly.GetCustomAttributes<PropertiesAttribute>() |> Seq.tryHead |> Option.map (fun t -> t.Config)
+              yield! getPropertiesOnDeclaringClasses x.Method.TypeInfo
+              yield x.GetFsCheckPropertyAttribute().Config |> Some]
+        
+        { config with Arbitrary = config.Arbitrary }
+        |> PropertyConfig.toConfig runner
+
     member private x.HandleException ex testResult failureSite =
         match ex with
         | :? ThreadAbortException -> Thread.ResetAbort()
@@ -147,32 +265,8 @@ and FsCheckTestMethod(mi : IMethodInfo, parentSuite : Test) =
         testResult.RecordException(x.FilterException <| ex, failureSite)
 
     member private x.RunTestMethod context testResult =
-        let parseReplay (str: string) =
-            //if someone sets this, we want it to throw if it fails
-            let split = str.Trim('(',')').Split([|","|], StringSplitOptions.RemoveEmptyEntries)
-            let seed = UInt64.Parse(split.[0])
-            let gamma = UInt64.Parse(split.[1])
-            let size = if split.Length = 3 then Some <| Convert.ToInt32(UInt32.Parse(split.[2])) else None
-            { Rnd = Rnd (seed,gamma); Size = size }
-        let attr = x.GetFsCheckPropertyAttribute()
         let testRunner = NunitRunner()
-        let config = Config.Default
-                           .WithMaxTest(attr.MaxTest)
-                           .WithMaxRejected(attr.MaxFail)
-                           .WithStartSize(attr.StartSize)
-                           .WithEndSize(attr.EndSize)
-                           .WithEvery(if attr.Verbose then Config.Verbose.Every else Config.Quick.Every)
-                           .WithEveryShrink(if attr.Verbose then Config.Verbose.EveryShrink else Config.Quick.EveryShrink)
-                           .WithArbitrary(attr.Arbitrary |> Array.toList)
-                           .WithReplay(
-                               match attr.Replay with 
-                               | null -> Config.Default.Replay 
-                               | s -> parseReplay s |> Some)
-                           .WithParallelRunConfig(
-                               if attr.Parallelism <= 0 
-                               then None 
-                               else Some { MaxDegreeOfParallelism = attr.Parallelism })
-                           .WithRunner(testRunner)
+        let config = x.Init(testRunner)
 
         let target = if not (isNull x.Fixture) then Some x.Fixture
                      elif x.Method.MethodInfo.IsStatic then None
@@ -180,7 +274,7 @@ and FsCheckTestMethod(mi : IMethodInfo, parentSuite : Test) =
         Check.Method(config, x.Method.MethodInfo, ?target = target)
         match testRunner.Result with
         | TestResult.Passed _ ->
-            if not attr.QuietOnSuccess then
+            if not config.QuietOnSuccess then
                 printfn "%s" (Runner.onFinishedToString "" testRunner.Result)
             testResult.SetResult(ResultState(TestStatus.Passed))
         | TestResult.Exhausted _ ->
