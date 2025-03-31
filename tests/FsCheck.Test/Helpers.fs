df2ce127c1bdbe0f812446c3210edb9bfc368e2a
Arialdo Martini <arialdo.martini@gmail.com>
2025-03-31 23:42:42 +0200

    Tests for memoize (#705)

--- tests/FsCheck.Test/Helpers.fs
+++ tests/FsCheck.Test/Helpers.fs
@@ -5,8 +5,6 @@ do()
 
 module Helpers = 
 
-    open System
-    open FsCheck
     open FsCheck.FSharp
 
     let sample n = Gen.sampleWithSize 1000 n
@@ -16,14 +14,3 @@ module Helpers =
     let isIn l elem = List.exists ((=) elem) l
 
     let assertTrue pred = if not pred then failwith "assertion failure"
-
-    open global.Xunit
-    open System.Threading
-    open System.Threading.Tasks
-
-    [<Fact>]
-    let ``memoize is thread-safe``() =
-        let f (a: int) = Thread.Sleep 100; a
-        let memoized = Internals.Common.memoize f
-        Array.init 100 (fun _ -> Action(fun _ -> memoized 1 |> ignore))
-        |> Parallel.Invoke
